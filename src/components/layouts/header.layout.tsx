563319607530108b0764361dc70de64c87b013c3
Muhammad Arizky <arizkymuhammad46@gmail.com>
2024-11-15 00:07:02 +0700

    feat: [skillset] add skill set layout in landing page

---
+++ src/components/layouts/header.layout.tsx
@@ -0,0 +1,40 @@
+import { Col, Image } from 'antd'
+import React from 'react'
+import { FlipWords } from '../ui/flip-words';
+
+export default function HeaderLayout(): React.JSX.Element {
+  const words = ["Fullstack Dev", "IT Support", "Web Developer"];
+
+  return (
+    <>
+      <Col className="flex items-center">
+        <Image
+          style={{
+            objectFit: 'cover',
+            objectPosition: '10% 40%',
+          }}
+          preview={false}
+          width={500}
+          height={500}
+          src="https://res.cloudinary.com/dpr6tofwx/image/upload/v1731404289/yjm16gd2pzh7hlsjhmav.jpg"
+        />
+      </Col>
+      <Col className="flex justify-items-center items-center" style={{
+        position: 'relative',
+        right: 40
+      }}>
+        <div>
+          <h1 className="text-white text-6xl leading-[60px] font-semibold">
+            I'm Muhammad Arizky,
+            <br /> A <FlipWords words={words} />
+            <br />
+            <span className="text-gray-400">based in Indonesia.</span>
+          </h1>
+          <p className="text-white mt-5 w-[830px] text-lg">I am a Full-stack developer with expertise in Laravel, Node.js and Next.Js.
+            I have experience in developing scalable, secure, and reliable web applications using various frameworks and technologies.
+            I enjoy solving complex problems and am always enthusiastic about learning new skills.</p>
+        </div>
+      </Col>
+    </>
+  )
+}
--- src/components/philosophy.tsx
+++ src/components/philosophy.tsx
@@ -18,7 +18,7 @@ export default function PhilosophyLayout() {
             objectPosition: '10% 40%',
           }}
           preview={false}
-          width={700}
+          width={600}
           height={500}
           src="https://images.unsplash.com/photo-1618388810903-840bb0d15ea5?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
         />
