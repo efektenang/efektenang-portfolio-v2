5f6862dadc65fc1129af7a978927050823779506
Muhammad Arizky <arizkymuhammad46@gmail.com>
2024-12-09 15:58:19 +0700

    feat: [components] create some component for contact us layout

--- src/components/card-hover-effect-demo.tsx
+++ src/components/card-hover-effect-demo.tsx
@@ -12,24 +12,24 @@ export const projects = [
     title: "Fullstack Web Developer",
     description:
       "Develop responsive and interactive user interfaces, manage server-side logic and backend services, integrate frontend elements with backend.",
-    link: "#skillset",
+    link: "#fullstack",
   },
   {
     title: "MVC Implementation",
     description:
       "Design pattern commonly used in software development to separate the codebase into three interconnected components, making it easier to manage, scale, and maintain the application",
-    link: "#skillset",
+    link: "#MVC",
   },
   {
     title: "IT Support",
     description:
       "Creating network planning and topology, implementing network security, and managing IP address allocation, VLANs, and routing",
-    link: "#skillset",
+    link: "#IT",
   },
   {
     title: "Building REST API",
     description:
       "Offering a strong foundation in building RESTFUL API, web development with a proven ability to drive efficiency, deliver successful outcomes.",
-    link: "#skillset",
+    link: "#api",
   },
 ];
